4d995e2b78e0ac74f9c67d271f9ab34dcbd86849
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2023-11-30 14:34:32 -0300

    fixed scheduler

--- src/olc_server/scheduler.py
+++ src/olc_server/scheduler.py
@@ -10,17 +10,17 @@ from .algorithms_olc import alg_olc
 app_rocketry = Rocketry()
 
 
-@app_rocketry.task(every("30 seconds"), execution="async")
-async def do_things():
+@app_rocketry.task(every("30 seconds"))
+def do_things():
     """
     Tarefa de teste
     """
     print("Scheduler health check")
 
-@app_rocketry.task(every("6 hours"))
-def update_trending_articles():
+@app_rocketry.task(every("6 hours"), execution="async")
+async def update_trending_articles():
     """
     Tarefa para atualizar os artigos em ascensão
     """
-    alg_olc.calculate_score()
+    await alg_olc.calculate_score()
     print("Updating trending articles")
